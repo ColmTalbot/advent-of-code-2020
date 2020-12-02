c8a181667170f507c122171dd5978a0b88d8cdd6
Colm Talbot <colm.talbot@ligo.org>
2020-12-02 13:18:58 -0500

    add package structure and main run script

---
+++ advent.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	advent "advent-2020/cmd"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var days []func()
+	days = append(days, advent.Day1)
+	lineBreak := strings.Repeat("=", 20)
+	for ii, function := range days {
+		fmt.Println(lineBreak)
+		fmt.Println("Day ", ii + 1)
+		fmt.Println(lineBreak)
+		function()
+	}
+	fmt.Println(lineBreak)
+}
