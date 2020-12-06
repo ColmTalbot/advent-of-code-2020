cc3dba86edd6a116b9b360712453f1b2e6f02f02
Colm Talbot <colm.talbot@ligo.org>
2020-12-06 11:04:00 -0500

    Add day 5

--- advent.go
+++ advent.go
@@ -19,6 +19,7 @@ func main() {
 	days = append(days, advent.Day2)
 	days = append(days, advent.Day3)
 	days = append(days, advent.Day4)
+	days = append(days, advent.Day5)
 
 	lineBreak := strings.Repeat("=", 20)
 
